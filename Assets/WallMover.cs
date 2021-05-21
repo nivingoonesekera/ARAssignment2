c9387c4e3d0424148311a9c931151eae34b41529
Joshua <joshuabond@gmail.com>
2021-05-21 15:49:25 +1000

    Added more wall types, and walls now cycle every 20 seconds until we run out of wall types (just enough time for the wall to reach the camera), says "yay" onscreen if wall never touches kyle.

--- Assets/WallMover.cs
+++ Assets/WallMover.cs
@@ -5,23 +5,28 @@ using UnityEngine.UI;
 
 public class WallMover : MonoBehaviour
 {
-
     public float moveSpeed = -0.5f;
+    private Vector3 startLocation;
     private float currentMoveSpeed = 0;
     public Text uiText;
     public float textTimer = 2f;
     private float textTimeTemp = 0f;
     public GameObject[] wallSets;
-    //private GameObject currentSet;
+    public float timeBetweenSets = 20f;
+    private float timeBetweenSetsTemp = 0f;
+    private GameObject currentSet;
+    private int currentSetIndex = 0;
+    private bool didCrumble = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        startLocation = this.transform.position;
         currentMoveSpeed = moveSpeed;
         //loads first wall set to move towards player
         if (wallSets.Length > 0)
         {            
-            GameObject currentSet = Instantiate(wallSets[0], this.transform);
+            currentSet = Instantiate(wallSets[currentSetIndex], this.transform);
             currentSet.transform.position = this.transform.position;
         }
     }
@@ -29,6 +34,14 @@ public class WallMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeBetweenSetsTemp += Time.deltaTime;
+        if (timeBetweenSetsTemp > timeBetweenSets)
+        {
+            timeBetweenSetsTemp -= timeBetweenSets;
+            NextSet();            
+        }
+
+
         this.transform.position += new Vector3(0, 0, currentMoveSpeed * Time.deltaTime);
         if (textTimeTemp > 0)
         {
@@ -48,16 +61,18 @@ public class WallMover : MonoBehaviour
             //this was mostly just to test collisions, before implementing wall crumble
             uiText.text = "Ouch!";
             textTimeTemp = textTimer;
-            currentMoveSpeed = 0f;
-            //trigger wall crumble
-            Crumble();
         }
+
+        currentMoveSpeed = 0f;
+        //trigger wall crumble
+        Crumble();
     }
 
     //Gets all grandchildren of wall and gives them a non-kinematic rigidbody affected by gravity 
     //this will make all the children act independently of the wall and fall to pieces
     private void Crumble()
     {
+        didCrumble = true;
         foreach (Transform childTransform in this.GetComponentInChildren<Transform>())
         {
             foreach (Transform setPieceTransform in childTransform.GetComponentInChildren<Transform>())
@@ -70,4 +85,25 @@ public class WallMover : MonoBehaviour
             }
         }
     }
+
+    void NextSet()
+    {
+        if (!didCrumble && uiText != null)
+        {
+            uiText.text = "Yay!";
+            textTimeTemp = textTimer;
+        }
+        didCrumble = false;
+
+        Destroy(currentSet);
+        currentSetIndex++;
+        if (currentSetIndex < wallSets.Length)
+        {
+            this.transform.position = startLocation;
+            currentSet = Instantiate(wallSets[currentSetIndex], this.transform);
+            currentSet.transform.position = this.transform.position;
+            currentMoveSpeed = moveSpeed;
+        }
+    }
+
 }
